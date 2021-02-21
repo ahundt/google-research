d01f168b8e9a5439360990b6fa84cc60b484a547
nimrobotics <nimrobotics@github.com>
2021-02-21 18:08:58 +0530

    skipped passing model_name arg to attention

--- ravens/ravens/agents/transporter.py
+++ ravens/ravens/agents/transporter.py
@@ -250,8 +250,9 @@ class OriginalTransporterAgent(TransporterAgent):
     self.attention = Attention(
         in_shape=self.in_shape,
         n_rotations=1,
-        preprocess=utils.preprocess,
-        model_name=model_name)
+        preprocess=utils.preprocess)
+        # model_name=model_name)
+
     self.transport = Transport(
         in_shape=self.in_shape,
         n_rotations=self.n_rotations,
